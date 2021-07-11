9825b0d4749b29b8d9ebb6e6f62436551c26bfea
Sergey <borzsonite@gmail.com>
2021-07-11 08:36:44 +0300

    entityManagerViaDao branch add

--- src/main/java/edu/segeyrozhkov/crud/app/dao/UserDao.java
+++ src/main/java/edu/segeyrozhkov/crud/app/dao/UserDao.java
@@ -4,17 +4,21 @@ import edu.segeyrozhkov.crud.app.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-public interface UserDao extends JpaRepository<User, Integer> {
-//
-//    public void addUser(User user);
-//
-//    public void updateUser(User user);
-//
-//    public void removeUser(int id);
-//
-//    public User getUserById(int id);
-//
-//    public List<User> userList();
+import javax.transaction.Transactional;
+import java.util.List;
+
+public interface UserDao {
+
+
+    public void addUser(User user);
+
+    public void updateUser(User user);
+
+    public void removeUser(int id);
+
+    public User getUserById(int id);
+
+    public List<User> userList();
 
 
 }
--- src/main/java/edu/segeyrozhkov/crud/app/service/UserServiceImp.java
+++ src/main/java/edu/segeyrozhkov/crud/app/service/UserServiceImp.java
@@ -1,37 +1,52 @@
 package edu.segeyrozhkov.crud.app.service;
 
+import edu.segeyrozhkov.crud.app.dao.UserDao;
 import edu.segeyrozhkov.crud.app.model.User;
 import edu.segeyrozhkov.crud.app.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 
 @Service
 public class UserServiceImp implements UserService {
+//    @Autowired
+//    private UserRepository repository;
+
     @Autowired
-    private UserRepository repository;
+    private UserDao userDao;
 
     @Override
+    @Transactional
     public void updateUser(User user) {
-        repository.save(user);
+       // repository.save(user);
+        userDao.addUser(user);
     }
 
     @Override
+    @Transactional
     public void removeUser(int id) {
-        repository.deleteById(id);
+       // repository.deleteById(id);
+        userDao.removeUser(id);
     }
 
     @Override
+    @Transactional
     public User getUserById(int id) {
-        Optional<User> user = repository.findById(id);
-        return user.orElse(null);
+//        Optional<User> user = repository.findById(id);
+//        return user.orElse(null);
+        return userDao.getUserById(id);
+
     }
 
     @Override
+    @Transactional
     public List<User> userList() {
-        return repository.findAll();
+//        return repository.findAll();
+        return userDao.userList();
     }
 }
 
